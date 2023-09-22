5ca859dd4d560f777647dba529e7b001e474d850
akshatra <akshatrauday@gmail.com>
2023-09-22 22:11:24 +0530

    app

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Topbar.css'
+
+
+function Topbar() {
+  return (
+    <div className='top'>
+    <div className="topleft">
+    left
+    </div>
+    <div className="topcenter">
+    center
+    </div>
+    <div className="topright">
+    right
+    </div>
+    </div>
+  )
+}
+
+export default Topbar
